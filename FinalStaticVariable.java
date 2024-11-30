79212e8685a262efed43b13e1eaa24ab063d28ed
Manish Khadka <mnzkhadka1996@gmail.com>
2024-11-30 22:32:29 +0545

    A final static variable is created where the value remains constant

---
+++ FinalStaticVariable.java
@@ -0,0 +1,7 @@
+public class FinalStaticVariable {
+    static double GRAVITY = 9.81;
+
+    public static void main(String[] args) {
+        System.out.println("The value of gravity : "+GRAVITY);
+    }
+}
